1aba76e1388a0c80aa2440339a1d603b0bf863cf
Francesco Ciannavei <fracianna5@gmail.com>
2026-04-09 18:09:29 +0200

    seo

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare(),
+  site: 'http://localhost:4321',
+  adapter: cloudflare(),
+  integrations: [sitemap()],
 });
