17fd0bd5267f5e200c5a9cafe1e1b85027b5703a
Francesco Ciannavei <fracianna5@gmail.com>
2026-04-15 16:12:15 +0200

    fix sitemap

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321',
+  site: 'https://docs-cloudflare-cdn.ciannavei.dev',
   adapter: cloudflare(),
   integrations: [sitemap()],
 });
